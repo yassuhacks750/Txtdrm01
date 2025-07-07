0768f7ec5554b75e1a585b239f120f7dea35958b
yassuhacks750 <yassudj8085231272@gmail.com>
2025-07-07 21:13:23 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,8 +7,12 @@ API_ID = int(environ.get("API_ID", "22849789"))
 API_HASH = environ.get("API_HASH", "0fc127c6055acd59f00ec6c229e1e3c4")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7330840404:AAE6tZtqpNCanwSNkkmw4zvdB_QTm6s7qfQ")
 OWNER = int(environ.get("OWNER", "7296271316"))
-AUTH_USERS = int(environ.get("AUTH_USERS", "7296271316"))
 CREDIT = "𝗬𝗔𝗦𝗛 👑"
+AUTH_USER = os.environ.get('AUTH_USERS', '7296271316').split(',')
+AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+if int(OWNER) not in AUTH_USERS:
+    AUTH_USERS.append(int(OWNER))
+  
 LOG_CHANNEL = "-1002631537674" # log group me bot ko admin narur banaye full rights ke sath
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
